b4325ee339ae027ba038f99eda5349329d3bfa6f
manodev1 <62101348+manodev1@users.noreply.github.com>
2024-03-12 10:10:14 +0530

    Create HelloWorld.java

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,20 @@
+package hello;
+
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+	    
+	    System.out.println("testing");
+	    
+	/*We need only the Password in this method or class*/
+	
+	/*No need to have SSH Keys*/
+	String keypassword = "ihaveremovedmypassword"; 
+  /*In case if we need the key */
+	
+	String apiKey="this-is-a-dummy-key";
+	int InvalidPasswordAttempt=0;
+  System.out.println("Hai");	    
+    }
+}
